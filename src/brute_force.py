1fec1d0dff488f82a35754d64f8aac74e0d144b1
valentinethrlr <v.thuerler@bluewin.ch>
2024-04-24 14:38:32 +0000

    debut

--- src/brute_force.py
+++ src/brute_force.py
@@ -1,5 +1,8 @@
 # Exercice https://courses.21-learning.com/runestone/books/published/oci-2224-donc/classic-problems/01-knapsack-short.html#force-brute
 
-from kp_instance import KPInstance
+from knapsack import KnapsackInstance, KnapsackSolver
 
-class 
+class BruteforceKnapsackSolver(KnapsackSolver):
+    
+    def solve(self):
+        
