5c3aea5a3fb69db380853812ba84b7904f7c5a08
valentinethrlr <v.thuerler@bluewin.ch>
2024-05-01 14:41:30 +0000

    recursion

--- src/recursive_solver.py
+++ src/recursive_solver.py
@@ -5,27 +5,48 @@ from knapsack import KnapsackInstance, KnapsackSolver
 class RecursiveKnapsackSolver(KnapsackSolver):
     """
     >>> kp = KnapsackInstance(W=[13, 13, 13, 10, 24, 11], V=[2600, 2600, 2600, 500, 4500, 960], C=50)
-    >>> bfs = RecursiveKnapsackSolver(kp)
-    >>> Xopt = bfs.solve()
+    >>> solver = RecursiveKnapsackSolver(kp)
+    >>> Xopt = solver.solve()
     >>> Xopt in [(0, 1, 1, 0, 1, 0), (1, 0, 1, 0, 1, 0), (1, 1, 0, 0, 1, 0)]
     True
-    >>> bfs.value(Xopt)
-    9700
-    >>> bfs.weight(Xopt)
-    50
-    >>> bfs.weight(Xopt) <= bfs._inst.C
-    True
+    >>> solver.optimal_solution()
 
     """
     
     def __init__(self, instance) -> None:
         # TODO: write the constructor by calling the parent class constructor
         super().__init__(instance)
-
-    def _knapsack(self, weights=None, values=None, capacity=None):
-        weights = self._inst.W if weights is None else weights
-        values = self._inst.W if values is None else values
         
+    def _knapsack(self, weights=None, values=None, capacity=None) -> int:
+        weights = self._inst.W if weights is None else weights
+        values = self._inst.V if values is None else values
+        capacity = self._inst.C if capacity is None else capacity
+        
+        if len(values) == 0:
+            return 0
+        
+        # résoudre sans l'objet
+        value_without = self._knapsack(weights[1:], values[1:], capacity)
+        
+        if capacity >= weights[0]:
+            # résoudre avec l'objet
+            value = self._knapsack(weights[1:], values[1:], capacity - weights[0])
+            value_with = value + values[0]
+        else:
+            value_with = -1
+        
+        # return meilleure solution
+        return max(value_without, value_with)
+            
+    def optimal_solution(self) -> int:
+        return self._knapsack()
+    
+    def solve(self) -> tuple[int, ...]:
+        # solve by brute force
+        
+        
+                    
+        return best_solution
         
 try:
     import doctest
