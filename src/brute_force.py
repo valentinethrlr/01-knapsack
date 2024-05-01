0cf75ca50b631a237da88aaf410c97b9cda4e596
valentinethrlr <v.thuerler@bluewin.ch>
2024-05-01 13:57:40 +0000

    create instance

--- src/brute_force.py
+++ src/brute_force.py
@@ -3,6 +3,36 @@
 from knapsack import KnapsackInstance, KnapsackSolver
 
 class BruteforceKnapsackSolver(KnapsackSolver):
+    """
+    >>> kp = KnapsackInstance(W=[13, 13, 13, 10, 24, 11], V=[2600, 2600, 2600, 500, 4500, 960], C=50)
+    >>> bfs = BruteforceKnapsackSolver(kp)
+    >>> Xopt = bfs.solve()
+    >>> Xopt in [(0, 1, 1, 0, 1, 0), (1, 0, 1, 0, 1, 0), (1, 1, 0, 0, 1, 0)]
+    True
+    >>> bfs.value(Xopt)
+    9700
+    >>> bfs.weight(Xopt)
+    50
+    >>> bfs.weight(Xopt) <= bfs._inst.C
+    True
+
+    """
     
-    def solve(self):
-        ...
+    def __init__(self, instance) -> None:
+        super().__init__(instance)
+
+    
+    def solve(self) -> tuple[int, ...]:
+        # solve by brute force
+        sol = (0,)
+        
+        # générer toutes les solutions
+        solutions: list[tuple[int, ...]] = []
+        
+        
+        
+        # éliminer toutes les solutions non faisables
+        
+        # trouver la meilleure solution
+        
+        return sol
