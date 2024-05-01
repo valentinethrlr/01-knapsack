b3f86187b4265605628d6760f4e0edd9fc50ecaa
valentinethrlr <v.thuerler@bluewin.ch>
2024-05-01 14:32:12 +0000

    recursive

---
+++ src/recursive_solver.py
@@ -0,0 +1,43 @@
+# Exercice https://courses.21-learning.com/runestone/books/published/oci-2224-donc/classic-problems/01-knapsack-short.html#force-brute
+
+from knapsack import KnapsackInstance, KnapsackSolver
+
+class RecursiveKnapsackSolver(KnapsackSolver):
+    """
+    >>> kp = KnapsackInstance(W=[13, 13, 13, 10, 24, 11], V=[2600, 2600, 2600, 500, 4500, 960], C=50)
+    >>> bfs = RecursiveKnapsackSolver(kp)
+    >>> Xopt = bfs.solve()
+    >>> Xopt in [(0, 1, 1, 0, 1, 0), (1, 0, 1, 0, 1, 0), (1, 1, 0, 0, 1, 0)]
+    True
+    >>> bfs.value(Xopt)
+    9700
+    >>> bfs.weight(Xopt)
+    50
+    >>> bfs.weight(Xopt) <= bfs._inst.C
+    True
+
+    """
+    
+    def __init__(self, instance) -> None:
+        # TODO: write the constructor by calling the parent class constructor
+        super().__init__(instance)
+
+    def _knapsack(self, weights=None, values=None, capacity=None):
+        weights = self._inst.W if weights is None else weights
+        values = self._inst.W if values is None else values
+        
+        
+try:
+    import doctest
+
+    doctest.testmod()
+except:
+    print("Unable to load doctests")
+
+def test():
+    kp = KnapsackInstance(W=[13, 13, 13, 10, 24, 11], V=[2600, 2600, 2600, 500, 4500, 960], C=50)
+    bfs = BruteforceKnapsackSolver(kp)
+    Xopt = bfs.solve()
+    
+    print("solution", Xopt)
+    
