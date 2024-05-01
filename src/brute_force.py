dfead1c1375b3c3dc4c561791cbb6ffb774f7e31
valentinethrlr <v.thuerler@bluewin.ch>
2024-05-01 14:22:35 +0000

    brute force

--- src/brute_force.py
+++ src/brute_force.py
@@ -19,43 +19,53 @@ class BruteforceKnapsackSolver(KnapsackSolver):
     """
     
     def __init__(self, instance) -> None:
+        # TODO: write the constructor by calling the parent class constructor
         super().__init__(instance)
 
-
-    def _all_solutions(n: int) -> list[tuple[int, ...]]:
-    
-        solutions = [None] * (2 ** n)
-        
+    def _all_solutions(self, n: int) -> list[tuple[int, ...]]:
+        solutions = [None] * 2 ** n
         for i in range(0, 2 ** n):
             bits: str = bin(i)[2:]
             k = n - len(bits)
             bits = k * '0' + bits
             solutions[i] = tuple([int(b) for b in bits])
-        
-        return solutions
 
+        return solutions
     
     def solve(self) -> tuple[int, ...]:
         # solve by brute force
+        
         n = self._inst.size
         
         # générer toutes les solutions
-        solutions: list[tuple[int, ...]] = self._all_solutions()
+        solutions: list[tuple[int, ...]] = self._all_solutions(n)
         
         # éliminer toutes les solutions non faisables
+        # solutions = [sol for sol in solutions if self.weight(sol) <= self._inst.C]
+        
+        best_solution = (0,)
+        best_value = 0
         # trouver la meilleure solution
+        for solution in solutions:
+            if self.weight(solution) <= self._inst.C:
+                value = self.value(solution)
+                if value > best_value:
+                    best_solution = solution
+                    best_value = value
+                    
+        return best_solution
         
-        current_max: int = 0
-        
-        for sol in solutions:
-            if self.value(sol) > current_max and self.weight(sol) <= self._inst.C:
-                current_max = self.value(sol)
-        
-        return sol
-    
 try:
     import doctest
 
     doctest.testmod()
 except:
     print("Unable to load doctests")
+
+def test():
+    kp = KnapsackInstance(W=[13, 13, 13, 10, 24, 11], V=[2600, 2600, 2600, 500, 4500, 960], C=50)
+    bfs = BruteforceKnapsackSolver(kp)
+    Xopt = bfs.solve()
+    
+    print("solution", Xopt)
+    
