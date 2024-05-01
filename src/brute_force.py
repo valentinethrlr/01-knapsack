277e03b5a07fa028952e7be25ae2a29fa6ff6768
valentinethrlr <v.thuerler@bluewin.ch>
2024-05-01 14:13:21 +0000

    brute force

--- src/brute_force.py
+++ src/brute_force.py
@@ -21,18 +21,41 @@ class BruteforceKnapsackSolver(KnapsackSolver):
     def __init__(self, instance) -> None:
         super().__init__(instance)
 
+
+    def _all_solutions(n: int) -> list[tuple[int, ...]]:
+    
+        solutions = [None] * (2 ** n)
+        
+        for i in range(0, 2 ** n):
+            bits: str = bin(i)[2:]
+            k = n - len(bits)
+            bits = k * '0' + bits
+            solutions[i] = tuple([int(b) for b in bits])
+        
+        return solutions
+
     
     def solve(self) -> tuple[int, ...]:
         # solve by brute force
-        sol = (0,)
+        n = self._inst.size
         
         # générer toutes les solutions
-        solutions: list[tuple[int, ...]] = []
-        
-        
+        solutions: list[tuple[int, ...]] = self._all_solutions()
         
         # éliminer toutes les solutions non faisables
-        
         # trouver la meilleure solution
         
+        current_max: int = 0
+        
+        for sol in solutions:
+            if self.value(sol) > current_max and self.weight(sol) <= self._inst.C:
+                current_max = self.value(sol)
+        
         return sol
+    
+try:
+    import doctest
+
+    doctest.testmod()
+except:
+    print("Unable to load doctests")
